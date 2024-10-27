57a65dfaa89c55b74e8a3941ab380cd368b64919
Julian Rodemann <julian.rodemann@freenet.de>
2024-10-27 14:48:19 -0400

    ...

--- benchmarks/experiments_banknote.R
+++ benchmarks/experiments_banknote.R
@@ -5,13 +5,10 @@ set.seed(2037420)
 N = 40
 
 share_setups = c(0.7,0.8,0.9)
-#n_setups = c(180,190,200)
 
 
 for (share_unlabeled in share_setups) {
- # for (n in n_setups){
     share_unlabeled %>% print
-   # n %>% print()
 
     
     try(
@@ -22,19 +19,8 @@ for (share_unlabeled in share_setups) {
       source(paste(getwd(),"/analyze/analyze_params.R", sep=""))
     #)
     
-    # print standard errors:
-    
-    # likelihood_params_sd %>% print
-    # 
-    # Bayes_crit_params_sd %>% print
-    # 
-    # Bayes_crit_reg_params_sd %>% print
-    # 
-    # pred_var_params_sd %>% print
-    # 
-    # prob_score_params_sd %>% print
-    
+
     
   }
-#}
+
 
